6db7786a571b1320d0d8cba7ef638e6f65f929a0
treeston <treeston.mmoc@gmail.com>
2016-02-27 20:17:27 +0100

    Gatewatcher, Berserk changes, respawn changes, and so on and so forth.

--- sql/custom/world/customboss_dps_1.sql
+++ sql/custom/world/customboss_dps_1.sql
@@ -2,27 +2,49 @@
 -- dps check: 2750 dps sustained
 -- weakening bar: 60 seconds normal, 15 seconds weakened (first weaken after 20 sec)
 -- enrage after 4 minutes 20 seconds (end of 4th weakened phase)
-SET @BASEENTRY = 100000;
+SET @BASECENTRY = 100000;
+SET @BASEOENTRY = 1000000;
 SET @BASECGUID = 1500000;
 SET @BASEOGUID = 1500000;
+SET @BASEGOSSIP = 60400;
+SET @BASETEXT = 16740000;
 
-DELETE FROM `creature_template` WHERE `entry` between @BASEENTRY and @BASEENTRY;
-INSERT INTO `creature_template` (`entry`,`modelid1`,`name`,`subname`,`minlevel`,`maxlevel`,`faction`,`speed_walk`,`speed_run`,`scale`,`rank`,`BaseAttackTime`,`BaseVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`type`,`type_flags`,`mingold`,`maxgold`,`ScriptName`,`MovementType`,`InhabitType`,`HealthModifier`,`ExperienceModifier`,`mechanic_immune_mask`,`flags_extra`) VALUES
-                             (@BASEENTRY,16174,"The Wrecker", "Abominable Abomination",83,83,     14,           2,       0.15, 1.5   ,     3,             850,             1,           1,           0,            0,     6,          76,        1,        1,"boss_custom_dps1",       0,            1,         123.139,                   0,            2147483647,       262484);
-					
-DELETE FROM `creature` WHERE `guid` between @BASECGUID and @BASECGUID;
-INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`movementtype`) VALUES
-                   (@BASECGUID,@BASEENTRY,169,     15,          1,        0,      -145.9,      2454.2,   92.007912,3.14159265359,             30,             0);
-				   
-DELETE FROM `creature_text` WHERE `entry` = @BASEENTRY;
+DELETE FROM `creature_template` WHERE `entry` between @BASECENTRY+0 and @BASECENTRY+1;
+INSERT INTO `creature_template` (`entry`,`modelid1`,`name`,`subname`,`minlevel`,`maxlevel`,`faction`,`npcflag`,`speed_walk`,`speed_run`,`scale`,`rank`,`BaseAttackTime`,`BaseVariance`,`unit_class`,`unit_flags`,`unit_flags2`,`type`,`type_flags`,`mingold`,`maxgold`,`ScriptName`,`MovementType`,`InhabitType`,`HealthModifier`,`ExperienceModifier`,`mechanic_immune_mask`,`flags_extra`) VALUES
+(@BASECENTRY+0,16174,"The Wrecker","Abominable Abomination",83,83,14,0,2,0.15,1.5,3,850,1,1,0,0,6,76,1,1,"boss_custom_dps1",0,1,123.139,0,2147483647,262484),
+(@BASECENTRY+1,27108,"Novarch","The Gate Watcher",83,83,35,1,2,2,0.1,3,0.5,1,1,770,0,5,135266324,0,0,"npc_custom_dps1_gatewatcher",0,1,50,0,2147483647,2);
+							 
+DELETE FROM `creature_text` WHERE `entry` = @BASECENTRY+0;
 INSERT INTO `creature_text` (`entry`,`groupid`,`id`,`text`,`type`,`probability`,`TextRange`) VALUES
-(@BASEENTRY, 1,0,"Little $n watch while I smash!",14,100,3),
-(@BASEENTRY, 2,0,"%s begins to charge the wall! Take it down before it can get there!",41,100,3),
-(@BASEENTRY, 3,0,"Tired. So...tired.",12,100,3),
-(@BASEENTRY, 4,0,"%s is weakened while taking a rest. Strike now!",41,100,3),
-(@BASEENTRY, 5,0,"Coming for you now, puny mortals!",14,100,3),
-(@BASEENTRY, 6,0,"%s charges at the barrier with renewed strength!",41,100,3),
-(@BASEENTRY, 7,0,"Wrecker tired of games. Now, smash!",14,100,3),
-(@BASEENTRY, 8,0,"%s begins to relentlessly pound the wall! Take it down!",41,100,3),
-(@BASEENTRY, 9,0,"Hmph. Friend will not help you here.",14,100,3),
-(@BASEENTRY,10,0,"Why... So tired....",12,100,3);
+(@BASECENTRY+0, 1,0,"Little $n watch while I smash!",14,100,3),
+(@BASECENTRY+0, 2,0,"%s begins to charge the wall! Take it down before it can get there!",41,100,3),
+(@BASECENTRY+0, 3,0,"Tired. So...tired.",12,100,3),
+(@BASECENTRY+0, 4,0,"%s is weakened while taking a rest. Strike now!",41,100,3),
+(@BASECENTRY+0, 5,0,"Coming for you now, puny mortals!",14,100,3),
+(@BASECENTRY+0, 6,0,"%s charges at the barrier with renewed strength!",41,100,3),
+(@BASECENTRY+0, 7,0,"Wrecker tired of games. Now, smash!",14,100,3),
+(@BASECENTRY+0, 8,0,"%s begins to relentlessly pound the wall! Take it down!",41,100,3),
+(@BASECENTRY+0, 9,0,"Hmph. Friend will not help you here.",14,100,3),
+(@BASECENTRY+0,10,0,"Why... So tired....",12,100,3);
+
+DELETE FROM `npc_text` WHERE `ID` between @BASETEXT+0 and @BASETEXT+1;
+INSERT INTO `npc_text` (`ID`,`text0_0`,`text0_1`) VALUES
+(@BASETEXT+0,"Hold, $c!$B$BJust beyond this gate stands an abomination of enormous size, ready to charge at my beleaguered companions that have retreated into the mountain beyond.$B$B$BFor now, it seems to have lost interest, but if you disturb its rest, it will certainly resume its attacks.$B$BI plead of you, $c. Do not enter this room.",""),
+(@BASETEXT+1,"Halt, $c.$B$BJust beyond this gate, a brave adventurer is battling to defeat a peaceful being that would've harmed nobody if it hadn't been disturbed.$B$BI warned them not to enter, but they just wouldn't listen.$B$B<The watcher sighs heavily.>$B$BThey never listen.","");
+
+DELETE FROM `gameobject_template` WHERE `entry` between @BASEOENTRY+0 and @BASEOENTRY+1;
+INSERT INTO `gameobject_template` (`entry`,`type`,`displayId`,`name`,`flags`,`size`,`Data0`,`Data1`,`Data2`,`ScriptName`) VALUES
+(@BASEOENTRY+0,0,4132,"boss_wrecker_gate01",4,1,0,0,30000,""),
+(@BASEOENTRY+1,0,9214,"bonus_boss_icewall",4,2,1,0,0,"");
+					
+DELETE FROM `creature` WHERE `guid` between @BASECGUID+0 and @BASECGUID+1;
+INSERT INTO `creature` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`modelid`,`position_x`,`position_y`,`position_z`,`orientation`,`spawntimesecs`,`movementtype`) VALUES
+(@BASECGUID+0,@BASECENTRY+0,169,1,1,0,-145.9,2454.2,92.008,3.14159265359,30,0),
+(@BASECGUID+1,@BASECENTRY+1,169,1,1,0,-94.71,2429.0,92.008,5.432591,     30,0);
+
+DELETE FROM `gameobject` WHERE `guid` between @BASEOGUID+0 and @BASEOGUID+3;
+INSERT INTO `gameobject` (`guid`,`id`,`map`,`spawnMask`,`phaseMask`,`position_x`,`position_y`,`position_z`,`orientation`,`rotation0`,`rotation1`,`rotation2`,`rotation3`,`spawntimesecs`,`state`) VALUES
+(@BASEOGUID+0,@BASEOENTRY+0,169,1,1,-110.434,2427.93,101.041,5.78446,0,0,0.246788,-0.969069,10,1),
+(@BASEOGUID+1,@BASEOENTRY+0,169,1,1,-110.434,2427.93,92.0079,5.78446,0,0,0.246788,-0.969069,10,1),
+(@BASEOGUID+2,@BASEOENTRY+1,169,1,1,-83.8265,2432.05,92.0079,4.43437,0,0,0.798265,-0.602306,10,1),
+(@BASEOGUID+3,@BASEOENTRY+1,169,1,1,-105.475,2407.02,92.0079,1.1349 ,0,0,0.537481, 0.843276,10,1);
--- src/server/scripts/Custom/boss_custom_dps1.cpp
+++ src/server/scripts/Custom/boss_custom_dps1.cpp
@@ -1,8 +1,15 @@
+#include <boost/format.hpp>
 #include "ScriptMgr.h"
 #include "ScriptedCreature.h"
+#include "ScriptedGossip.h"
 #include "Player.h"
 #include "SpellAuras.h"
 
+static const uint32 BASECENTRY = 100000;
+static const uint32 BASEOENTRY = 1000000;
+static const uint32 BASEGOSSIP = 60400;
+static const uint32 BASETEXT = 16740000;
+static const WorldLocation ENTRY_POINT(169, -76.59f, 2410.39f, 92.008f, 2.6429f);
 enum WreckerTalk
 {
     YELL_ENCOUNTER_BEGIN        = 1, // Little creature watch while I smash!
@@ -28,11 +35,12 @@ enum Spells
     SPELL_SLEEPY_VISUAL         =  6606,
     SPELL_BANISH                = 42354,
     SPELL_SPEED_UP              = 62375,
-    SPELL_REMORSELESS_WINTER    = 68981,
-    SPELL_BERSERK               = 59620,
+    SPELL_BERSERK_AURA          = 70084,
+    SPELL_BERSERK               = 41924,
     SPELL_ICY_GRIP              = 70177,
     SPELL_ICY_CHAINS            = 29991,
-    SPELL_CHARGE                = 74399
+    SPELL_CHARGE                = 74399,
+    SPELL_TELEPORT_VISUAL       = 64446
 };
 
 enum Events
@@ -42,15 +50,30 @@ enum Events
     EVENT_WAKE_UP,
     EVENT_BEGIN_WALKING,
     EVENT_ICY_GRIP,
-    EVENT_CHARGE_WALL
+    EVENT_CHARGE_WALL,
+    EVENT_POWER_DOWN
 };
 
-enum Misc
+enum Data
 {
-    NPC_BUNNY  = 22515
+    DATA_IS_ENGAGED = 1
 };
 
-static float const AGGRO_DISTANCE = 60.0f;
+enum GossipMenuEntries
+{
+    GOSSIP_WRECKER_DIFFICULTY = BASETEXT+0,
+    GOSSIP_WRECKER_BUSY = BASETEXT+1
+};
+
+enum Entries
+{
+    NPC_WRECKER = BASECENTRY + 0,
+    NPC_NOVARCH = BASECENTRY + 1,
+
+    GO_GATE = BASEOENTRY + 0
+};
+
+static float const AGGRO_DISTANCE = 40.0f;
 
 static uint8 const numWeaken = 4;
 Position weakenPositions[numWeaken+1] =
@@ -74,12 +97,13 @@ public:
 
     struct boss_custom_dps1AI : public ScriptedAI
     {
-        boss_custom_dps1AI(Creature* creature) : ScriptedAI(creature), _weakenCount(0), _speedupCount(0), _timeSinceDamage(0) { }
+        boss_custom_dps1AI(Creature* creature) : ScriptedAI(creature), _startTime(0), _weakenCount(0), _speedupCount(0), _timeSinceDamage(0) { }
 
         void InitializeAI()
         {
             me->Respawn();
             SetCombatMovement(false);
+            Reset();
         }
 
         void Reset()
@@ -87,11 +111,20 @@ public:
             me->SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_ONESHOT_NONE);
             me->SetCorpseDelay(5);
             me->SetReactState(REACT_PASSIVE);
+            me->setPowerType(POWER_ENERGY);
+            me->SetMaxPower(POWER_ENERGY, 100);
+            me->SetPower(POWER_ENERGY, 20);
             _playerGUID = ObjectGuid::Empty;
         }
 
         void ResetEncounter()
         {
+            if (_playerGUID)
+                if (Player* player = ObjectAccessor::GetPlayer(*me, _playerGUID))
+                {
+                    player->CastSpell(player, SPELL_TELEPORT_VISUAL, true);
+                    player->TeleportTo(ENTRY_POINT);
+                }
             _playerGUID = ObjectGuid::Empty;
             me->DespawnOrUnsummon();
         }
@@ -101,11 +134,13 @@ public:
             if (IsEncounterActive())
                 return;
 
+            if (who->GetEntry() == NPC_NOVARCH)
+                return;
+
             Player* pWho = who->GetCharmerOrOwnerPlayerOrPlayerItself();
 
             if (!pWho)
             {
-                std::cout << "no player engaging" << std::endl;
                 ResetEncounter();
                 return;
             }
@@ -113,6 +148,7 @@ public:
             if (pWho->IsGameMaster())
                 return;
 
+            _startTime = getMSTime();
             _playerGUID = pWho->GetGUID();
             _weakenCount = 0;
             _speedupCount = 1;
@@ -127,7 +163,14 @@ public:
             _events.Reset();
             _events.ScheduleEvent(EVENT_BEGIN_WALKING, 3 * IN_MILLISECONDS);
             _events.ScheduleEvent(EVENT_SLEEPY, 20 * IN_MILLISECONDS);
+            me->SetPower(POWER_ENERGY, 20);
             _events.ScheduleEvent(EVENT_SPEED_UP, 10 * IN_MILLISECONDS);
+            _events.ScheduleEvent(EVENT_POWER_DOWN, 1 * IN_MILLISECONDS);
+
+            std::list<GameObject*> gateGOs;
+            me->GetGameObjectListWithEntryInGrid(gateGOs, GO_GATE, 80.0f);
+            for (GameObject* gate : gateGOs)
+                gate->SetGoState(GO_STATE_READY);
         }
 
         void DamageTaken(Unit* source, uint32& damage) override
@@ -135,7 +178,6 @@ public:
            Player* pSource = source ? source->GetCharmerOrOwnerPlayerOrPlayerItself() : nullptr;
            if (!pSource || pSource->GetGUID() != _playerGUID)
            {
-               std::cout << "Anticheat prevented " << damage << " damage." << std::endl;
                damage = 0;
                if (pSource && !pSource->HasAura(SPELL_BANISH))
                {
@@ -146,11 +188,21 @@ public:
            _timeSinceDamage = 0;
         }
 
-        inline bool IsEncounterActive() { return !_playerGUID.IsEmpty(); }
+        inline bool IsEncounterActive() const { return !_playerGUID.IsEmpty(); }
+
+        uint32 GetData(uint32 data) const override
+        {
+            switch (data)
+            {
+                case DATA_IS_ENGAGED:
+                    return IsEncounterActive() ? 1u : 0u;
+                default:
+                    return 0u;
+            }
+        }
 
         void EnterEvadeMode(EvadeReason why = EVADE_REASON_OTHER) override
         {
-            std::cout << "EnterEvadeMode" << std::endl;
             ResetEncounter();
         }
 
@@ -177,9 +229,8 @@ public:
             if (!UpdateVictim())
                 return;
 
-            if (_timeSinceDamage > 10 * IN_MILLISECONDS)
+            if (_weakenCount < numWeaken && _timeSinceDamage > 10 * IN_MILLISECONDS)
             {
-                std::cout << "damage timeout" << std::endl;
                 ResetEncounter();
                 return;
             }
@@ -224,15 +275,16 @@ public:
                             _events.ScheduleEvent(EVENT_BEGIN_WALKING, 2 * IN_MILLISECONDS);
                             _events.ScheduleEvent(EVENT_SPEED_UP, 10 * IN_MILLISECONDS);
                             _events.ScheduleEvent(EVENT_SLEEPY, MINUTE * IN_MILLISECONDS);
+                            me->SetPower(POWER_ENERGY, MINUTE);
+                            _events.ScheduleEvent(EVENT_POWER_DOWN, 1 * IN_MILLISECONDS);
                         }
                         else
                         {
                             Talk(YELL_BERSERK);
-                            Talk(EMOTE_BERSERK);
-                            DoCastAOE(SPELL_BERSERK);
-                            DoCastAOE(SPELL_REMORSELESS_WINTER);
 
-                            _events.ScheduleEvent(EVENT_CHARGE_WALL, 3 * IN_MILLISECONDS);
+                            me->SetPower(POWER_ENERGY, 100);
+
+                            _events.ScheduleEvent(EVENT_CHARGE_WALL, 2 * IN_MILLISECONDS);
                             _events.ScheduleEvent(EVENT_ICY_GRIP, 10 * IN_MILLISECONDS);
                         }
                         break;
@@ -251,19 +303,44 @@ public:
                         break;
 
                     case EVENT_CHARGE_WALL:
-                        if (Creature* bunny = DoSummon(NPC_BUNNY, weakenPositions[numWeaken], 5 * IN_MILLISECONDS, TEMPSUMMON_TIMED_DESPAWN))
-                            DoCast(bunny, SPELL_CHARGE);
-                        me->SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_ONESHOT_ATTACK1H);
+                        Talk(EMOTE_BERSERK);
+                        me->GetMotionMaster()->MoveCharge(weakenPositions[numWeaken].GetPositionX(), weakenPositions[numWeaken].GetPositionY(), weakenPositions[numWeaken].GetPositionZ(), 42.0f, 2);
+                        break;
+
+                    case EVENT_POWER_DOWN:
+                        me->SetPower(POWER_ENERGY, _events.GetTimeUntilEvent(EVENT_SLEEPY) / IN_MILLISECONDS);
+                        if (me->GetPower(POWER_ENERGY))
+                            _events.ScheduleEvent(EVENT_POWER_DOWN, 1 * IN_MILLISECONDS);
                         break;
                 }
         }
 
+        void MovementInform(uint32 movetype, uint32 id)
+        {
+            if (id == 2)
+            {
+                DoCastAOE(SPELL_BERSERK);
+                DoCastAOE(SPELL_BERSERK_AURA);
+                me->SetUInt32Value(UNIT_NPC_EMOTESTATE, EMOTE_ONESHOT_ATTACK1H);
+            }
+        }
+
         void JustDied(Unit* killer) override
         {
+            Player* pKiller = killer->GetCharmerOrOwnerPlayerOrPlayerItself();
+            if (!pKiller || pKiller->GetGUID() != _playerGUID)
+            {
+                ResetEncounter();
+                return;
+            }
+
+            uint32 killTime = GetMSTimeDiffToNow(_startTime);
             Talk(YELL_DEATH);
+            me->Talk(Trinity::StringFormat("%%s has been defeated by $n in %d.%d seconds!", killTime / IN_MILLISECONDS, killTime % IN_MILLISECONDS), CHAT_MSG_MONSTER_EMOTE, LANG_UNIVERSAL, 50000.0f, pKiller);
             ScriptedAI::JustDied(killer);
         }
 
+        uint32 _startTime;
         ObjectGuid _playerGUID;
         uint8 _weakenCount;
         uint8 _speedupCount;
@@ -271,7 +348,61 @@ public:
         uint32 _timeSinceDamage;
     };
 };
+
+class npc_custom_dps1_gatewatcher : public CreatureScript
+{
+public:
+    npc_custom_dps1_gatewatcher() : CreatureScript("npc_custom_dps1_gatewatcher") { }
+
+    inline static Creature* _wrecker(Creature const* self) { return self->FindNearestCreature(NPC_WRECKER, 100.0f, true); }
+
+    CreatureAI* GetAI(Creature* creature) const override
+    {
+        return new npc_custom_dps1_gatewatcherAI(creature);
+    }
+
+    struct npc_custom_dps1_gatewatcherAI : public ScriptedAI
+    {
+        npc_custom_dps1_gatewatcherAI(Creature* creature) : ScriptedAI(creature) { }
+    };
+
+    bool OnGossipSelect(Player* player, Creature* me, uint32 /*sender*/, uint32 action) override
+    {
+        switch (action)
+        {
+            case GOSSIP_ACTION_INFO_DEF+1:
+            {
+                player->PlayerTalkClass->SendCloseGossip();
+                std::list<GameObject*> gateGOs;
+                me->GetGameObjectListWithEntryInGrid(gateGOs, GO_GATE, 30.0f);
+                for (GameObject* gate : gateGOs)
+                    gate->SetGoState(GO_STATE_ACTIVE);
+                break;
+            }
+        }
+        return true;
+    }
+
+    bool OnGossipHello(Player* player, Creature* me) override
+    {
+        if (player->IsGameMaster())
+            player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "[GM] Open the door for me.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+        if (Creature* wrecker = _wrecker(me))
+        {
+            if (!wrecker->AI()->GetData(DATA_IS_ENGAGED))
+            {
+                player->ADD_GOSSIP_ITEM(GOSSIP_ICON_CHAT, "I will enter.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+                player->PlayerTalkClass->SendGossipMenu(GOSSIP_WRECKER_DIFFICULTY, me->GetGUID());
+                return true;
+            }
+        }
+        player->PlayerTalkClass->SendGossipMenu(GOSSIP_WRECKER_BUSY, me->GetGUID());
+        return true;
+    }
+};
+
 void AddSC_boss_custom_dps1()
 {
     new boss_custom_dps1();
+    new npc_custom_dps1_gatewatcher();
 }
